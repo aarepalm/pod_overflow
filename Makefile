e77ed49031a917cca6fd30df420fc61a0a597fb6
Aare Palm <aarepalm@gmail.com>
2019-06-13 13:48:17 +0300

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,3 @@
+test_overflow: main.cpp
+	g++ -o test_overflow main.cpp -std=c++17
+
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <limits>
+#include <string>
+#include <iostream>
+#include <bitset>
+
+static const unsigned char BITS_IN_BYTE = 8;
+
+int main()
+{
+  std::cout << "sizeof int is " << sizeof(int) << std::endl;
+
+  std::cout << "max   int is " << std::numeric_limits<int>::max() << std::endl;
+  std::cout << "max   int is " << std::bitset<sizeof(int)*BITS_IN_BYTE>(std::numeric_limits<int>::max()) << std::endl;
+  
+  std::cout << "max+1 int is " << std::numeric_limits<int>::max() + 1 << std::endl;
+  std::cout << "max+1 int is " << std::bitset<sizeof(int)*BITS_IN_BYTE>(std::numeric_limits<int>::max() + 1) << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "min   int is " << std::numeric_limits<int>::min() << std::endl;
+  std::cout << "min   int is " << std::bitset<sizeof(int)*BITS_IN_BYTE>(std::numeric_limits<int>::min()) << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "min-1 int is " << std::numeric_limits<int>::min() - 1 << std::endl;
+  std::cout << "min-1 int is " << std::bitset<sizeof(int)*BITS_IN_BYTE>(std::numeric_limits<int>::min() -1) << std::endl;
+  std::cout << std::endl;
+
+  return 0;
+}
